68030281c1a9e97c18c24f9d5e8c8a136e92aa30
쓰리원 <90826931+ilil1@users.noreply.github.com>
2023-05-28 15:21:25 +0900

    Header 기능 구현

--- presentation/src/main/java/com/example/presentation/ui/MainScreen.kt
+++ presentation/src/main/java/com/example/presentation/ui/MainScreen.kt
@@ -1,11 +1,18 @@
 package com.example.presentation.ui
 
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.presentation.R
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -13,6 +20,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.presentation.ui.theme.ShoppingComposeTheme
+import com.example.presentation.viewmodel.MainViewModel
 
 @Preview(showBackground = true)
 @Composable
@@ -20,6 +28,13 @@ fun DefaultPreview() {
     ShoppingComposeTheme {
         MainScreen()
     }
+}
+
+
+sealed class MainNavigationItem(var route: String, val icon: ImageVector, var name: String) {
+    object Main : MainNavigationItem("Main", Icons.Filled.Home, "Main")
+    object Category : MainNavigationItem("Category", Icons.Filled.Star, "Category")
+    object MyPage : MainNavigationItem("MyPage", Icons.Filled.AccountBox, "MyPage")
 }
 
 /**
@@ -30,9 +45,11 @@ fun DefaultPreview() {
  */
 @Composable
 fun MainScreen() {
+    val viewModel = hiltViewModel<MainViewModel>()
     val scaffoldState = rememberScaffoldState()
     val navController = rememberNavController()
     Scaffold(
+        topBar = { Header(viewModel) },
         bottomBar = {
             MainBottomNavigationBar(navController)
         }
@@ -42,10 +59,29 @@ fun MainScreen() {
     }
 }
 
-sealed class MainNavigationItem(var route: String, var name: String) {
-    object Main : MainNavigationItem("Main", "Main")
-    object Category : MainNavigationItem("Category", "Category")
-    object MyPage : MainNavigationItem("MyPage", "MyPage")
+/**
+ * TopAppBar는 일반적으로 세 가지 주요 컴포넌트로 구성됩니다:
+ * navigationIcon: 왼쪽에 표시되는 아이콘. 주로 드로어 메뉴나 뒤로 가기 버튼이 위치합니다.
+ * title: 중앙에 표시되는 텍스트.
+ * actions: 오른쪽에 표시되는 아이콘들.
+ * 이 부분은 일반적으로 검색, 공유, 설정 등의 액션을 수행하는 아이콘들로 구성됩니다.
+ */
+
+@Composable
+fun Header(viewModel: MainViewModel) {
+    TopAppBar(
+        title = { Text("My App") },
+        actions = {
+            IconButton(onClick = {
+                viewModel.openSearchForm()
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "SearchIcon"
+                )
+            }
+        }
+    )
 }
 
 @Composable
@@ -87,7 +123,7 @@ fun MainBottomNavigationBar(navController: NavHostController) {
             BottomNavigationItem(
                 icon = {
                     Icon(
-                        painterResource(id = R.drawable.ic_launcher_foreground),
+                        item.icon,
                         item.route
                     )
                 },
